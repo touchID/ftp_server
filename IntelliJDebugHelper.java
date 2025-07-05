790fd171ca1a02cb8adc87c4a7c456017c09ab5b
touchid <ios.7@qq.com>
2025-07-05 19:50:47 +0800

    某时间 自动停止

--- IntelliJDebugHelper.java
+++ IntelliJDebugHelper.java
@@ -3,6 +3,9 @@ import java.awt.MouseInfo;
 import java.awt.Point; 
 import java.awt.Robot; 
 import java.util.Random; 
+import java.time.LocalTime; 
+import java.time.ZoneId; 
+import java.time.ZonedDateTime; 
 
 public class IntelliJDebugHelper { 
     public static void main(String[] args) { 
@@ -11,6 +14,14 @@ public class IntelliJDebugHelper {
             Robot robot = new Robot(); 
             Random random = new Random(); 
             while (true) { 
+                // 获取北京时间 
+                ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Shanghai")); 
+                LocalTime now = zonedDateTime.toLocalTime(); 
+                // 判断是否在 12 点 - 13 点 或 18 点之后 
+                if ((now.isAfter(LocalTime.of(12, 0)) && now.isBefore(LocalTime.of(13, 0))) || now.isAfter(LocalTime.of(18, 0))) { 
+                    Thread.sleep(60000); // 处于停止时间段，每分钟检查一次时间 
+                    continue; 
+                } 
                 // 生成 60000 到 120000 之间的随机数 
                 int randomInterval = random.nextInt(60001) + 60000; 
                 Thread.sleep(randomInterval); 
