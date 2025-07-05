77117c7d74f6a0f08462f72e73c036fddfd1cc72
touchid <ios.7@qq.com>
2025-07-05 19:41:29 +0800

    随机秒

--- IntelliJDebugHelper.java
+++ IntelliJDebugHelper.java
@@ -2,15 +2,18 @@ import java.awt.AWTException;
 import java.awt.MouseInfo; 
 import java.awt.Point; 
 import java.awt.Robot; 
+import java.util.Random; 
 
 public class IntelliJDebugHelper { 
     public static void main(String[] args) { 
         try { 
             // 创建 Robot 对象 
             Robot robot = new Robot(); 
+            Random random = new Random(); 
             while (true) { 
-                // 每隔 60 秒移动一次鼠标 
-                Thread.sleep(1000); 
+                // 生成 60000 到 120000 之间的随机数 
+                int randomInterval = random.nextInt(60001) + 60000; 
+                Thread.sleep(randomInterval); 
                 // 获取当前鼠标位置 
                 Point point = MouseInfo.getPointerInfo().getLocation(); 
                 // 相对当前位置移动鼠标 1 个像素 
